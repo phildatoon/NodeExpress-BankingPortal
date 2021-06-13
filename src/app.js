85d4b8e8516f770366b57db32f7a9878593e977e
Phil Datoon <Philip_Bryan_Datoon@manulife.ca>
2021-06-13 18:18:42 +0800

    Implement module 3: File Handling and Routing

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
 const path = require('path');
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
@@ -9,8 +9,30 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
+const accountData = fs.readFileSync(path.join(__dirname, '/json/accounts.json'), 'UTF-8');
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(path.join(__dirname, '/json/users.json'), 'UTF-8');
+const users = JSON.parse(userData);
+
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Index'});
+  res.render('index', { title: 'Account Summary', accounts: accounts });
+});
+
+app.get('/savings', (req, res) => {
+  res.render('account', { account: accounts.savings });
+});
+
+app.get('/checking', (req, res) => {
+  res.render('account', { account: accounts.checking });
+});
+
+app.get('/credit', (req, res) => {
+  res.render('account', { account: accounts.credit });
+});
+
+app.get('/profile', (req, res) => {
+  res.render('profile', { user: users[0] });
 });
 
 const port = 3000;
