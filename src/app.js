8e30d184a0f3a3cf79bbc7e5d5c7a125a282f636
Phil Datoon <Philip_Bryan_Datoon@manulife.ca>
2021-06-13 19:18:55 +0800

    Implement module 6: Using the Express Router

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const express = require('express');
 const { accounts, users, writeJSON } = require('./data');
+const accountRoutes = require('./routes/accounts.js');
+const servicesRoutes = require('./routes/services.js');
+
 
 const app = express();
 
@@ -14,47 +17,12 @@ app.get('/', (req, res) => {
   res.render('index', { title: 'Account Summary', accounts: accounts });
 });
 
-app.get('/savings', (req, res) => {
-  res.render('account', { account: accounts.savings });
-});
-
-app.get('/checking', (req, res) => {
-  res.render('account', { account: accounts.checking });
-});
-
-app.get('/credit', (req, res) => {
-  res.render('account', { account: accounts.credit });
-});
-
 app.get('/profile', (req, res) => {
   res.render('profile', { user: users[0] });
 });
 
-app.get('/transfer', (req, res) => {
-  res.render('transfer');
-})
-
-app.post('/transfer', (req, res) => {
-  accounts[req.body.from].balance -= req.body.amount;
-  accounts[req.body.to].balance += parseInt(req.body.amount, 10);
-
-  writeJSON();
-
-  res.render('transfer', { message: 'Transfer Completed' });
-});
-
-app.get('/payment', (req, res) => {
-  res.render('payment', { account: accounts.credit });
-})
-
-app.post('/payment', (req, res) => {
-  accounts.credit.balance -= req.body.amount;
-  accounts.credit.available += parseInt(req.body.amount, 10);
-
-  writeJSON();
-
-  res.render('payment', { message: 'Payment Successful', account: accounts.credit });
-})
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
 
 const port = 3000;
 app.listen(port, (err) => {
