6dd7295b59e29a1a6f5835b16b1b93f847217b2a
Phil Datoon <Philip_Bryan_Datoon@manulife.ca>
2021-06-13 18:47:26 +0800

    Implement module 4: Handling Form Data

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 const express = require('express');
+const { ap } = require('ramda');
 
 const app = express();
 
@@ -8,6 +9,7 @@ app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: true}));
 
 const accountData = fs.readFileSync(path.join(__dirname, '/json/accounts.json'), 'UTF-8');
 const accounts = JSON.parse(accountData);
@@ -34,6 +36,34 @@ app.get('/credit', (req, res) => {
 app.get('/profile', (req, res) => {
   res.render('profile', { user: users[0] });
 });
+
+app.get('/transfer', (req, res) => {
+  res.render('transfer');
+})
+
+app.post('/transfer', (req, res) => {
+  accounts[req.body.from].balance -= req.body.amount;
+  accounts[req.body.to].balance += parseInt(req.body.amount, 10);
+
+  let accountsJSON = JSON.stringify(accounts, null, 3);
+  fs.writeFileSync(path.join(__dirname, '/json/accounts.json'), accountsJSON, 'UTF-8');
+
+  res.render('transfer', { message: 'Transfer Completed' });
+});
+
+app.get('/payment', (req, res) => {
+  res.render('payment', { account: accounts.credit });
+})
+
+app.post('/payment', (req, res) => {
+  accounts.credit.balance -= req.body.amount;
+  accounts.credit.available += parseInt(req.body.amount, 10);
+
+  let accountsJSON = JSON.stringify(accounts, null, 3);
+  fs.writeFileSync(path.join(__dirname, '/json/accounts.json'), accountsJSON, 'UTF-8');
+
+  res.render('payment', { message: 'Payment Successful', account: accounts.credit });
+})
 
 const port = 3000;
 app.listen(port, (err) => {
